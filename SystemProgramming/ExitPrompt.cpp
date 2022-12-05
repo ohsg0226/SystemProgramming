b388b9cf7418056f9c13ea2cf36873004f98249d
ohsg0226 <ohsg0226@naver.com>
2022-12-05 21:35:03 +0900

    Add start echo "string" 명령어 기능 추가

--- SystemProgramming/ExitPrompt.cpp
+++ SystemProgramming/ExitPrompt.cpp
@@ -12,17 +12,34 @@
 
 TCHAR ERROR_CMD[] = _T("'%s' is not executable program. \n");
 
-int CmdProcessing(void);
+int CmdProcessing(int);
+int CmdReadTokenize(void);
 TCHAR* StrLower(TCHAR*);
+TCHAR cmdString[STR_LEN];
+TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
+TCHAR seps[] = _T(" ,\t\n");
+TCHAR* next_token = NULL;
 
-int main(int agrc, TCHAR *argv[])
+int _tmain(int argc, TCHAR* argv[])
 {
     _tsetlocale(LC_ALL, _T("Korean"));
 
-    DWORD isExit;
+    if (argc > 2) {
+        for (int i = 1; i < argc; i++)
+            _tcscpy_s(cmdTokenList[i - 1], argv[i]);
+
+        CmdProcessing(argc - 1);
+    }
+
+    DWORD isExit = NULL;
     while (1) {
-        isExit = CmdProcessing();
-        if (isExit == TRUE) {
+        int tokenNum = CmdReadTokenize();
+        if (tokenNum == 0)
+            continue;
+
+        isExit = CmdProcessing(tokenNum);
+        if (isExit == TRUE)
+        {
             _fputts(_T("명령어 처리를 종료합니다. \n"), stdout);
             break;
         }
@@ -30,42 +47,79 @@ int main(int agrc, TCHAR *argv[])
     return 0;
 }
 
-TCHAR cmdString[STR_LEN];
-TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
-TCHAR seps[] = _T(" ,\t\n");
-TCHAR* next_token = NULL;
+int CmdProcessing(int tokenNum)
+{
+    BOOL isRun;
+    STARTUPINFO si = { 0, };
+    PROCESS_INFORMATION pi;
 
-int CmdProcessing(void) {
-    _fputts(_T("Best command prompt>>"), stdout);
+    TCHAR cmdStringWithOptions[STR_LEN] = { 0, };
+    TCHAR optString[STR_LEN] = { 0, };
+
+    si.cb = sizeof(si);
+    if (!_tcscmp(cmdTokenList[0], _T("exit")))
+    {
+        return TRUE;
+    }
+    else if (!_tcscmp(cmdTokenList[0], _T("start"))) {
+        if (tokenNum > 1) {
+            for (int i = 1; i < tokenNum; i++)
+                _stprintf_s(optString, _T("%s %s"), optString, cmdTokenList[i]);
+
+            _stprintf_s(cmdStringWithOptions, _T("%s %s"), _T("Test.exe"), optString);
+        }
+        else
+            _stprintf_s(cmdStringWithOptions, _T("%s"), _T("Test.exe"));
+
+        isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+    else if (!_tcscmp(cmdTokenList[0], _T("echo")))
+    {
+        for (int i = 1; i < tokenNum; i++)
+            _stprintf_s(optString, _T("%s %s"), optString, cmdTokenList[i]);
+
+        _tprintf(_T("echo message:%s \n"), optString);
+    }
+    else
+    {
+        _tcscpy_s(cmdStringWithOptions, cmdTokenList[0]);
+
+        for (int i = 1; i < tokenNum; i++)
+            _stprintf_s(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
+
+        isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+
+        if (isRun == FALSE)
+            _tprintf(ERROR_CMD, cmdTokenList[0]);
+    }
+
+    return 0;
+}
+  
+
+int CmdReadTokenize(void)
+{
+    
+    _fputts(_T("Best command prompt>> "), stdout);
     _getts_s(cmdString);
 
     TCHAR* token = _tcstok_s(cmdString, seps, &next_token);
+
     int tokenNum = 0;
-    while (token != NULL) {
+
+    while (token != NULL)
+    {
         _tcscpy_s(cmdTokenList[tokenNum++], StrLower(token));
-        token = _tcstok_s(NULL, seps, &next_token);;
+        token = _tcstok_s(NULL, seps, &next_token);
     }
 
-    if (!_tcscmp(cmdTokenList[0], _T("exit"))) {
-        return TRUE;
-    }
-    else if (!_tcscmp(cmdTokenList[0], _T("instruction1"))) {
-
-    }
-    else if (!_tcscmp(cmdTokenList[0], _T("instruction2"))) {
-    }
-    else {
-        STARTUPINFO si = { 0, };
-        PROCESS_INFORMATION pi;
-        
-        si.cb = sizeof(si);
-        BOOL isRun = CreateProcess(NULL, cmdTokenList[0], NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
-        
-        if (isRun == FALSE) {
-            _tprintf(ERROR_CMD, cmdTokenList[0]);
-        }
-    }
-    return 0;
+    return tokenNum;
 }
 
 TCHAR* StrLower(TCHAR* pStr) {
